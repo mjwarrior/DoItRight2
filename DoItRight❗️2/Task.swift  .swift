1fb65e8d188579e9dc093fc58392e782a3580b90
marcstreeter <mstreeter@gmail.com>
2016-12-07 15:38:41 -0600

    Finished Lecture 58 section 6 added exclamation mark  and added a few tasks ❗️

---
+++ DoItRight❗️2/Task.swift  .swift
@@ -0,0 +1,17 @@
+ //
+//  Task.swift  .swift
+//  DoItRight❗️2
+//
+//  Created by Marc Streeter on 12/7/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import Foundation
+
+ class Task {
+
+var name = ""
+var important = false
+    
+    
+ }
--- DoItRight❗️2/ViewController.swift
+++ DoItRight❗️2/ViewController.swift
@@ -14,9 +14,14 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     @IBOutlet weak var tableView: UITableView!
     
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+   
+   tasks = makeTasks()
    
    tableView.dataSource = self
    tableView.delegate = self
@@ -24,14 +29,40 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
          let cell = UITableViewCell()
-        cell.textLabel?.text = "Hello"
+        let task = tasks[indexPath.row]
+        if task.important {
+                cell.textLabel?.text = "❗️\(task.name)"
+        }else{
+                cell.textLabel?.text = task.name
+        }
+        
+  
         return cell
     }
     
+    func makeTasks() -> [Task] {
+     let task1 = Task()
+        task1.name = "Learn how to make iphone applications"
+        task1.important = true
+        
+        let task2 = Task()
+        task2.name = "Learn How to make websites"
+        task2.important = true
+
+        let task3 = Task()
+        task3.name = "Learn how to make animations and videogames"
+        task3.important = true
+        
+        return[task1,task2,task3]
+
+    }
+
+
+
 }
 
