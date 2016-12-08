3109087568b690ca4d0399f71a95d881fc30afd7
marcstreeter <mstreeter@gmail.com>
2016-12-08 17:44:24 -0600

    I finished lecture 62 section 6 and I added a reload data and pop view controller and whenever you press add it goes back and shows you that you have added a task

--- DoItRight❗️2/CreateTaskViewController.swift
+++ DoItRight❗️2/CreateTaskViewController.swift
@@ -33,7 +33,8 @@ class CreateTaskViewController: UIViewController {
         //Add new task to array in previous viewController
     
     previousVC.tasks.append(task)
-        
+    previousVC.tableView.reloadData()
+    navigationController!.popViewController(animated: true)
     }
     
     }
