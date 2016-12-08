3af763bdaeed01d29f05c46d64ed6333c18c5730
marcstreeter <mstreeter@gmail.com>
2016-12-08 16:49:15 -0600

    I finished lecture 60 in section 6 and added and add button some labels a textfield and a switch and I made another view controller and turned the textfield the button and the switch into code

---
+++ DoItRight❗️2/CreateTaskViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CreateTaskViewController.swift
+//  DoItRight❗️2
+//
+//  Created by Marc Streeter on 12/7/16.
+//  Copyright © 2016 Developer Warriors. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    
+    @IBOutlet weak var importantSwitch: UISwitch!
+   
+    @IBOutlet weak var taskNameTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func addTapped(_ sender: AnyObject) {
+    }
+    
+    }
